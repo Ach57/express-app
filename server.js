4a1a80e8ea22e6c224df60ecd936e1dd928ac048
Achraf <155260097+Ach57@users.noreply.github.com>
2024-08-11 22:04:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/find4Digits',(req,res)=>{
 
 
 
-const port = process.env.PORT||3000;
+const port = process.env.PORT||3000; 
 app.listen(port, ()=>{
     console.log(`Server running at http://localhost:${port}`);
 })
